04318e8980d247c2a35489284c10a9b98daa9139
argenis616 <argenis616@ciencias.unam.mx>
2019-12-04 11:19:27 -0600

    Se agrega parametro tcp en socket

--- src/client.py
+++ src/client.py
@@ -2,12 +2,14 @@ import socket
 
 host = "localhost"
 port = 9999
-socket1 = socket.socket()
-socket1.connect((host,port))
-oracion = raw_input("Con que usuario: ")
-socket1.send(oracion)
-num = socket1.recv(1024)
-print "la oracion", oracion, "tienen caracteres:", num
-tiempo = raw_input("Presione enter para terminar...")
-
-socket1.close()
+socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(5)
+try:
+    socket1.connect((host,port))
+    oracion = raw_input("Con que usuario: ")
+    socket1.send(oracion)
+    num = socket1.recv(1024)
+    print "la oracion", oracion, "tienen caracteres:", num
+    tiempo = raw_input("Presione enter para terminar...")
+finally:
+    socket1.close()
--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ import SocketServer
 class MiTopHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         self.inicio = self.request.recv(1024)
+        print("{} wrote:".format(self.client_address[0]))
         if self.inicio == "1":
             s = "Bienvenido"
             self.request.send(s)
